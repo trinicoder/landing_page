1e877fd0502a4ff529266e94705b2413a0ecbc56
V.Silochan <varoonx@gmail.com>
2021-06-10 14:39:28 -0400

    animated mobile menu (bugs)

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+btnHamburger.addEventListener('click', function () {
+    console.log('open hamburger');
+
+    if(header.classList.contains('open')) { //close Hamburger menu
+       header.classList.remove('open');
+       overlay.classList.remove('fade-in')
+       overlay.classList.add('fade-out')
+    }
+    else { // Open hamburger menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out')
+        overlay.classList.add('fade-in')
+    }
+    
+})
